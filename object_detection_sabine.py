f86219e740e6d6b0895a375e3954c5542df58e38
SabineVirginia <sabe_fabe@msn.com>
2021-09-28 09:58:37 +0200

    My functions

--- object_detection_sabine.py
+++ object_detection_sabine.py
@@ -8,9 +8,15 @@ import random
 import collections
 import pandas as pd
 import cv2 as cv
+import tensorflow as tf
 from matplotlib import patches
 import xml.etree.ElementTree as ET
 from matplotlib import pyplot as plt
+from object_detection.utils import visualization_utils as viz_utils
+import numpy as np
+from PIL import Image
+import warnings
+warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
 
 ####################################################################################################
 # XML to Pandas DataFrame for image and annotation inspection
@@ -241,3 +247,181 @@ def plot_sample_bndbox(path_name, nr_images, bnd_box_df):
             ax.add_patch(patches.Rectangle((obj_x, obj_y), obj_width, obj_height, fill=False, edgecolor='lime', lw=0.5))
 
     return
+
+####################################################################################################
+# Jpg to Numpy array for TensorFlow graph
+
+def jpg_to_np(path):
+    """Load an image from file into a numpy array.
+
+    Puts image into numpy array to feed into tensorflow graph.
+    Note that by convention we put it into a numpy array with shape
+    (height, width, channels), where channels=3 for RGB.
+
+    Args:
+      path: the file path to the image
+
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3)
+    """
+    return np.array(Image.open(path))
+
+####################################################################################################
+# Detection of objects in an image
+
+@tf.function
+def detect_fn(image, model_for_detection):
+    """Detect objects in image.
+
+    Args:
+        image: TensorFlow tensor of an image
+        model_for_detection: TensorFlow object detection model
+
+    Return:
+        detections: dictionary with detection information    
+    """
+
+    image, shapes = model_for_detection.preprocess(image)
+    prediction_dict = model_for_detection.predict(image, shapes)
+    detections = model_for_detection.postprocess(prediction_dict, shapes)
+
+    return detections
+
+####################################################################################################
+# Save np array with image and all detected objects
+
+def save_image_np_with_all_detections(path, model_for_detection, category_index):
+    """This function converts the image under path into a numpy array, detects objects in the image with the model_for_detection,
+    and saves the numpy array with the image and detected objects.
+    
+    Args:
+        path: path to jpg
+        model_for_detection: TensorFlow model loaded with model_builder
+        category_index: dictionary mapping categories to labels
+
+    Return:
+        None: saves numpy array with the image and detected objects
+    """
+
+    img_np = jpg_to_np(path)
+    input_tensor = tf.convert_to_tensor(np.expand_dims(img_np, 0), dtype=tf.float32)
+
+    detections = detect_fn(input_tensor, model_for_detection)
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    label_id_offset = 1
+    image_np_with_detections = img_np.copy()
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                                                image_np_with_detections,
+                                                detections['detection_boxes'],
+                                                detections['detection_classes']+label_id_offset,
+                                                detections['detection_scores'],
+                                                category_index,
+                                                use_normalized_coordinates=True,
+                                                max_boxes_to_draw=200,
+                                                min_score_thresh=.30,
+                                                agnostic_mode=False,
+                                                skip_labels=True,
+                                                skip_scores=True)
+    np.save('image_np_with_detections.npy', image_np_with_detections)
+
+    return
+
+####################################################################################################
+# Save np array with image and one detected objects
+
+def save_image_np_with_one_detection(path, model_for_detection, category_index, obj_nr):
+    """This function converts the image under path into a numpy array, detects one object, which is selected by obj_nr,
+    in the image with the model_for_detection, and saves the numpy array with the image and the detected object.
+    
+    Args:
+        path: path to jpg
+        model_for_detection: TensorFlow model loaded with model_builder
+        category_index: dictionary mapping categories to labels
+
+    Return:
+        None: saves numpy array with the image and the detected object
+    """
+
+    img_np = jpg_to_np(path)
+    input_tensor = tf.convert_to_tensor(np.expand_dims(img_np, 0), dtype=tf.float32)
+
+    detections = detect_fn(input_tensor, model_for_detection)
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    one_detection = {}
+    one_detection['detection_boxes'] = np.array([detections['detection_boxes'][obj_nr]])
+    one_detection['detection_classes'] = np.array([detections['detection_classes'][obj_nr]])
+    one_detection['detection_scores'] = np.array([detections['detection_scores'][obj_nr]])
+    one_detection['num_detections'] = 1
+    label_id_offset = 1
+    image_np_with_detections = img_np.copy()
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                                                image_np_with_detections,
+                                                one_detection['detection_boxes'],
+                                                one_detection['detection_classes']+label_id_offset,
+                                                one_detection['detection_scores'],
+                                                category_index,
+                                                use_normalized_coordinates=True,
+                                                max_boxes_to_draw=200,
+                                                min_score_thresh=.30,
+                                                agnostic_mode=False,
+                                                skip_labels=True,
+                                                skip_scores=True)
+    np.save('image_np_with_detections.npy', image_np_with_detections)
+
+    return
+
+####################################################################################################
+# Object detection from jpg-file
+
+def detection_from_jpg(jpg_file_path, detection_model):
+    """This function detects objects with a TensorFlow-model.
+    Args:
+        jpg_file_path: path to jpg
+        model_for_detection: TensorFlow model loaded with model_builder
+
+    Return:
+        all_detections: dictionary with information about detected objects
+    """
+
+    img_np = jpg_to_np(jpg_file_path)
+    input_tensor = tf.convert_to_tensor(np.expand_dims(img_np, 0), dtype=tf.float32)
+
+    all_detections = detect_fn(input_tensor, detection_model)
+    num_detections = int(all_detections.pop('num_detections'))
+    all_detections = {key: value[0, :num_detections].numpy() for key, value in all_detections.items()}
+    all_detections['num_detections'] = num_detections
+    all_detections['detection_classes'] = all_detections['detection_classes'].astype(np.int64)
+
+    return all_detections
+
+####################################################################################################
+# Select detections above a certain threshold score
+
+def detection_selection(tf_detections, threshold):
+    """This function selects the objects that were detected with TensorFlow that have a detection score above the threshold.
+    
+    Args:
+        tf_detections: dictionary with information about detected objects
+        threshold: float
+        
+    Return:
+        detections: dictionary with information about detected objects that have detection score above threshold"""
+
+    detections = {}
+    detections['detection_boxes'] = tf_detections['detection_boxes'][tf_detections['detection_scores'] > threshold]
+    detections['detection_scores'] = tf_detections['detection_scores'][tf_detections['detection_scores'] > threshold]
+    detections['detection_classes'] = tf_detections['detection_classes'][tf_detections['detection_scores'] > threshold]
+    detections['raw_detection_boxes'] = tf_detections['raw_detection_boxes'][tf_detections['detection_scores'] > threshold]
+    detections['raw_detection_scores'] = tf_detections['raw_detection_scores'][tf_detections['detection_scores'] > threshold]
+    detections['detection_multiclass_scores'] = tf_detections['detection_multiclass_scores'][tf_detections['detection_scores'] > threshold]
+    detections['detection_anchor_indices'] = tf_detections['detection_anchor_indices'][tf_detections['detection_scores'] > threshold]
+    detections['num_detections'] = sum(tf_detections['detection_scores'] > threshold).item()
+
+    return detections
